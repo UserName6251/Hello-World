5e68bf0b36764f174a6baa02813f735861f58353
Ethan <89792003+UserName6251@users.noreply.github.com>
2021-09-02 09:32:33 -0500

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I Learned!");
+            int i = 0;
+            do
+            {
+                Console.WriteLine("i = {0}", i);
+                i++;
+
+            } while (i < 5);
+        }
+    }
+}
